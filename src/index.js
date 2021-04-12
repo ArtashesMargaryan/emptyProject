e55ad8ed9c2ade5fbacb119ec846e52eb0f6c941
Artashes <ArtashesMargaryanseryojayi@gamil.com>
2021-04-13 01:17:08 +0400

    part1

--- src/index.js
+++ src/index.js
@@ -1,82 +1,157 @@
-export TWO_PI=2*Math.PI
-export class Main {
-  constructor() {
-    this.canvas = document.querySelector(`canvas`);
-    this.context = canvas.getContext('2d');
-    this.width, this.height;
-    this.mouse;
-    this.dots=[]
-    this.canvas.addEventListener('mousemove', setPos);
-    this.canvas.addEventListener('mousedown', isDown, this);
-    this.canvas.addEventListener('mouseup', isDown, this);
-    this.buildConfig();
-    this.init();
-    this.buildCell();
-  }
+console.warn('mtav');
 
-  init() {
-    width = canvas.width = innerWidth;
-    height = canvas.height = innerHeight;
-    this.mouse = { x: width / 2, y: height / 2, down: false };
-  }
+(() => {
+  const config = {
+    dotMinRad: 6,
+    dotMaxRad: 20,
+    masFactor: 0.002,
+    defColor: `rgba(250,10,30,0.9)`,
+  };
+  const canvas = document.querySelector(`canvas`);
+  const ctx = canvas.getContext(`2d`);
+  let w, h, mouse, dots;
 
-  buildConfig() {
-    this.config = {
-      dotMinRed: 6,
-      dotMaxRed: 20,
-      massGravity: 0.002,
-      color: 'rgba(250,10,10,0.8)',
-    };
-  }
+  class Dot {
+    constructor() {
+      this.pos = { x: mouse.x, y: mouse.y };
+      this.vel = { x: 0, y: 0 };
+      this.rad = random(config.dotMinRad, config.dotMaxRad);
+      this.mass = this.rad * config.masFactor;
+      this.color = config.defColor;
+    }
 
-  buildCell() {
-    const cell = new Cell(this);
-  }
+    draw() {
+      this.createCircle(this.pos.x, this.pos.y, this.rad, true, this.color);
+      this.createCircle(this.pos.x, this.pos.y, this.rad, false, config.color);
+    }
 
-  setPos({ pointerX, pointerY }) {
-    (this.mouse.x = pointerX), (this.mouse.y = pointerY);
-  }
-
-  loop() {
-    this.context.clearRect(0,0,this.width,this.height)
-    if(this.mouse.down){
-      this.dots
+    createCircle(x, y, rad, fill, color) {
+      ctx.fillStyle = ctx.strokeStyle = color;
+      ctx.beginPath();
+      ctx.arc(x, y, red, 0, Math.PI);
+      ctx.closePath();
+      fill ? ctx.fill() : ctx.stroke();
     }
   }
-
-  isDown() {
-    this.mouse.down = !this.mouse.down;
-  }
-}
-
-export class Cell {
-  constructor(context) {
-    this.context=context
-    this.pos = { x: context.mouse.x, y: context.mouse.y };
-    this.vel = { x: 0, y: 0 };
-    this.radius = random(context.config.dotMinRed, context.config.dotMaxRed);
-    this.mass = this.radius * context.config.massGravity;
-    this.color = context.config.color;
+  function random(min, max) {
+    return Math.random() * (max - min) + min;
   }
 
-  draw() {
-    this.createCircle(this.pos.x, this.pos.y, this.radius, true, this.color);
-    this.createCircle(this.pos.x, this.pos.y, this.radius, false, this.color);
+  function init() {
+    w = canvas.width = innerWidth;
+    h = canvas.height = innerHeight;
+    mouse = { x: w / 2, y: h / 2, down: false };
+    dots = [];
   }
 
-  createCircle(x, y, radius, fill, color) {
-    const ctx=this.context.context
-    ctx.fillStyle=ctx.strokeStyle=color
-    ctx.beginPath();
-    ctx.arc(x,y,red,0,TWO_PI);
-    ctx.closePath();
-    fill?ctx.fill():ctx.stroke();
-
+  function loop() {
+    ctx.clearRect(0, 0, w, h);
+    if (mouse.down) {
+      dots.push(new Dot());
+    }
+    dots.map((e) => {
+      e.draw();
+    });
+    window.requestAnimationFrame(loop);
   }
-}
 
-export function random(min, max) {
-  return Math.random() * (max - min) + min;
-}
+  init();
+  loop();
 
-const game = new Main();
+  function setPos({ layerX, layerY }) {
+    [mouse.x, mouse.y] = [layerX, layerY];
+  }
+
+  function isDown() {
+    mouse.down = !mouse.down;
+  }
+
+  canvas.addEventListener(`mousemove`, setPos);
+  canvas.addEventListener(`mousedown`, isDown);
+  canvas.addEventListener(`mouseup`, isDown);
+})();
+// class Main {
+//   constructor() {
+//     this.canvas = document.querySelector(`canvas`);
+//     this.context = canvas.getContext(`2d`);
+//     this.width, this.height;
+//     this.mouse;
+//     this.dots = [];
+//     this.canvas.addEventListener('mousemove', this.setPos);
+//     this.canvas.addEventListener('mousedown', this.isDown, this);
+//     this.canvas.addEventListener('mouseup', this.isDown, this);
+//     this.buildConfig();
+//     this.init();
+//     this.loop();
+//   }
+
+//   init() {
+//     this.dots = [];
+//     this.width = canvas.width = innerWidth;
+//     this.height = canvas.height = innerHeight;
+//     this.mouse = { x: this.width / 2, y: this.height / 2, down: false };
+//   }
+
+//   buildConfig() {
+//     this.config = {
+//       dotMinRed: 6,
+//       dotMaxRed: 20,
+//       massGravity: 0.002,
+//       color: 'rgba(250,10,10,0.8)',
+//     };
+//   }
+
+//   buildCell() {
+//     const cell = new Cell(this);
+//     return cell;
+//   }
+
+//   setPos({ pointerX, pointerY }) {
+//     (this.mouse.x = pointerX), (this.mouse.y = pointerY);
+//   }
+
+//   loop() {
+//     this.context.clearRect(0, 0, this.width, this.height);
+//     if (this.mouse.down) {
+//       this.dots.push(this.buildCell());
+//     }
+//     this.dots.map((cell) => cell.draw());
+//     window.requestAnimationFrame(this.loop);
+//   }
+
+//   isDown() {
+//     this.mouse.down = !this.mouse.down;
+//   }
+// }
+
+// class Cell {
+//   constructor(context) {
+//     this.context = context;
+//     this.pos = { x: context.mouse.x, y: context.mouse.y };
+//     this.vel = { x: 0, y: 0 };
+//     this.radius = random(context.config.dotMinRed, context.config.dotMaxRed);
+//     this.mass = this.radius * context.config.massGravity;
+//     this.color = context.config.color;
+//   }
+
+//   draw() {
+//     this.createCircle(this.pos.x, this.pos.y, this.radius, true, this.color);
+//     this.createCircle(this.pos.x, this.pos.y, this.radius, false, this.color);
+//   }
+
+//   createCircle(x, y, radius, fill, color) {
+//     const ctx = this.context.context;
+//     ctx.fillStyle = ctx.strokeStyle = color;
+//     ctx.beginPath();
+//     ctx.arc(x, y, red, 0, 2 * Math.PI);
+//     ctx.closePath();
+//     fill ? ctx.fill() : ctx.stroke();
+//   }
+// }
+
+// function random(min, max) {
+//   return Math.random() * (max - min) + min;
+// }
+
+// console.warn('hasa');
+// const game = new Main();
